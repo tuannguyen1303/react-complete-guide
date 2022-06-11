eaa2a5b468549c0c3ce10c01ac1c27ec8b4bbb82
tuannguyen1303 <tuan1331210006@gmail.com>
2022-06-11 12:53:05 +0700

    prepair dummy data for display on gridview

--- src/App.js
+++ src/App.js
@@ -3,32 +3,7 @@ import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const DUMMY_EXPENSE = [
-  {
-    title: "Car Insurance 1",
-    amount: 100,
-    date: new Date(2020, 8, 2),
-    id: "e1",
-  }, // new Date(year, month, day)
-  {
-    title: "Car Insurance 2",
-    amount: 200,
-    date: new Date(2020, 9, 2),
-    id: "e2",
-  },
-  {
-    title: "Car Insurance 3",
-    amount: 300,
-    date: new Date(2021, 10, 2),
-    id: "e3",
-  },
-  {
-    title: "Car Insurance 4",
-    amount: 400,
-    date: new Date(2022, 11, 2),
-    id: "e4",
-  },
-];
+import { DUMMY_EXPENSE } from "./dummy-data/dummy_expense";
 
 const App = () => {
   const [expenses, setExpense] = useState(DUMMY_EXPENSE);
---
+++ src/dummy-data/dummy_expense.js
@@ -0,0 +1,26 @@
+export const DUMMY_EXPENSE = [
+  {
+    title: "Car Insurance 1",
+    amount: 100,
+    date: new Date(2020, 8, 2),
+    id: "e1",
+  },
+  {
+    title: "Car Insurance 2",
+    amount: 200,
+    date: new Date(2020, 9, 2),
+    id: "e2",
+  },
+  {
+    title: "Car Insurance 3",
+    amount: 300,
+    date: new Date(2021, 10, 2),
+    id: "e3",
+  },
+  {
+    title: "Car Insurance 4",
+    amount: 400,
+    date: new Date(2022, 11, 2),
+    id: "e4",
+  },
+];
